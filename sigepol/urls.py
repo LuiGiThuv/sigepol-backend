aea928f5aaa9d43c8f1717a2493bc27e0ea0d607
LuiGiThuv <131642446+LuiGiThuv@users.noreply.github.com>
2025-12-17 14:55:15 -0300

    Permitir HEAD requests en root endpoint

--- sigepol/urls.py
+++ sigepol/urls.py
@@ -18,9 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
 
-@require_http_methods(["GET"])
 def api_root(request):
     """API Root endpoint"""
     return JsonResponse({
